86ba49f7cc544bba362608d89a77671952de117e
mrOwlChan <mr.scandiacus@gmail.com>
2022-05-21 22:23:36 +0700

    Instalasi Template Bootstraps Carousel

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return view('templates.mainNavbar');
+    // return view('templates.mainNavbar');
+    return view('templates.homeNavbar');
 });
